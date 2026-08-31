f5f60af730f1f0d531ad0ece3ede5220834e6d71
bojorquezja <bojorquez.ja@gmail.com>
2026-08-31 18:57:23 -0500

    problema 1

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+#crea un aplicativo con un menú repetitivo con 4 opciones 
+# 1) agregar donde pedirá nombre y apellido y concatenados 
+# los agregará en una lista 
+# 2) listar que mostrará todos los nombres completos de esa lista y 
+# 3) eliminar que pedirá un texto buscará ese texto y se le encuentra
+# lo eliminará 
+# 4) será para salir 
